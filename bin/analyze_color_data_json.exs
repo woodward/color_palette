66b814f11e9acc51e5d087e105f89345428ae5d2
Greg Woodward <woodward@users.noreply.github.com>
2024-04-08 12:47:42 -0700

    Script to download color data, along with the color data and a script to analyze it

---
+++ bin/analyze_color_data_json.exs
@@ -0,0 +1,77 @@
+#!/usr/bin/env elixir
+
+Mix.install([
+  {:jason, "~> 1.4"}
+])
+
+import IO.ANSI
+
+color_data_file = Path.join(__DIR__, "../lib/color_names/color_data.json")
+color_data = color_data_file |> File.read!() |> Jason.decode!(keys: :atoms)
+
+# color_data = color_data |> Enum.take(3)
+
+_names =
+  color_data
+  |> Enum.map(fn color_data ->
+    name = color_data.name.value
+    # IO.puts(light_yellow() <> name <> reset())
+    name
+  end)
+
+with_multiple_names =
+  color_data
+  |> Enum.filter(fn color_data ->
+    String.contains?(color_data.name.value, " / ")
+  end)
+
+_multiple_names = with_multiple_names |> Enum.map(& &1.name.value)
+
+names_to_colors =
+  color_data
+  |> Enum.reduce(%{}, fn color_data, acc ->
+    name = color_data.name.value
+    Map.update(acc, name, [color_data], fn colors -> [color_data | colors] end)
+  end)
+  |> Enum.map(fn {name, colors} ->
+    {name, colors |> Enum.sort_by(& &1.name.distance)}
+  end)
+  |> Enum.into(%{})
+
+blueberry = Map.get(names_to_colors, "Blueberry")
+# IO.inspect(blueberry)
+
+IO.puts(light_blue() <> "Blueberry colors:" <> reset())
+
+blueberry
+|> Enum.map(fn color_data ->
+  IO.puts(light_yellow() <> "#{color_data.name.distance}   #{color_data.hex.value}" <> reset())
+end)
+IO.puts(" ")
+
+names_with_more_than_one_color =
+  names_to_colors
+  |> Enum.filter(fn {_name, colors} -> length(colors) > 1 end)
+
+IO.puts(
+  light_green() <>
+    "There are #{length(names_with_more_than_one_color)} names with more than one color." <>
+    reset()
+)
+
+names_with_more_than_one_color_simplified =
+  names_with_more_than_one_color
+  |> Enum.map(fn {name, colors} ->
+    {name, colors |> Enum.map(& &1.hex.value)}
+  end)
+
+duplicate_hexes =
+  names_with_more_than_one_color_simplified
+  |> Enum.reduce(MapSet.new(), fn {_, hexes}, acc ->
+    hexes
+    |> Enum.reduce(acc, fn hex, acc ->
+      MapSet.put(acc, hex)
+    end)
+  end)
+
+IO.puts(light_yellow() <> "There are #{MapSet.size(duplicate_hexes)} duplicate hexes." <> reset())
---
+++ bin/download_color_data.exs
@@ -0,0 +1,41 @@
+#!/usr/bin/env elixir
+
+Mix.install([
+  {:jason, "~> 1.4"},
+  {:req, "~> 0.4"}
+])
+
+import IO.ANSI
+
+IO.puts(light_yellow() <> "Downloading color data..." <> reset())
+
+# Sample URL:
+# https://www.thecolorapi.com/id?hex=00ffff&format=json
+
+ansi_color_code_filename = Path.join(__DIR__, "../lib/color_names/ansi_color_codes.json")
+color_codes = ansi_color_code_filename |> File.read!() |> Jason.decode!(keys: :atoms)
+
+IO.puts(light_green() <> "There are #{length(color_codes)} color codes" <> reset())
+
+code = color_codes |> hd()
+IO.puts(light_blue() <> "First color code: #{inspect(code)}" <> reset())
+
+# color_codes = color_codes |> Enum.take(3)
+sleep_time = 400
+
+color_data =
+  color_codes
+  |> Enum.reduce({[], 0}, fn code, {data, index} ->
+    IO.puts("==========================================")
+    IO.puts(light_yellow() <> "Index: #{index}.  Hex: #{code.hex}" <> reset())
+    result_body = Req.get!("https://www.thecolorapi.com/id?hex=#{code.hex}&format=json").body
+    Process.sleep(sleep_time)
+    {[result_body] ++ data, index + 1}
+  end)
+  |> elem(0)
+  |> Enum.reverse()
+
+IO.puts(light_green() <> "Downloaded #{length(color_data)} color data items" <> reset())
+IO.puts(light_yellow() <> "Writing color data to color_data.json" <> reset())
+File.write!("color_data.json", Jason.encode!(color_data, pretty: true))
+IO.puts(light_green() <> "Color data writen." <> reset())
