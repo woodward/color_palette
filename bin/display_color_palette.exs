098a4be884a732e24420feb9921596d9cffa3534
Greg Woodward <woodward@users.noreply.github.com>
2024-04-09 06:22:37 -0700

    Create a script to display all of the color names

---
+++ bin/display_color_palette.exs
@@ -0,0 +1,30 @@
+#!/usr/bin/env elixir
+
+Mix.install([
+  {:color_palette, path: Path.join(__DIR__, "../")}
+])
+
+defmodule Display do
+  import ColorPalette
+
+  def stats do
+    colors = ColorPalette.colors() |> Map.keys() |> length()
+    IO.puts(yellow() <> "\nThere are #{colors} unique color names\n" <> reset())
+  end
+
+  def all do
+    ColorPalette.colors()
+    |> Enum.sort()
+    |> Enum.map(fn {name, color} ->
+      hex = color |> List.first() |> Map.get(:hex) |> Map.get(:value)
+
+      IO.puts(
+        apply(ColorPalette, name, []) <>
+          "This is color :#{String.pad_trailing(Atom.to_string(name), 21)}  Hex value: #{hex}" <> reset()
+      )
+    end)
+  end
+end
+
+Display.all()
+Display.stats()
