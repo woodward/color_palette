4443ef0cb97fa606dfa17aba0c867612cad51142
Greg Woodward <woodward@users.noreply.github.com>
2024-04-10 16:16:15 -0700

    Create a function to group ansi color codes by color group

--- test/color_palette/color_group_test.exs
+++ test/color_palette/color_group_test.exs
@@ -21,4 +21,26 @@ defmodule ColorPalette.ColorGroupTest do
              ]
     end
   end
+
+  describe "ansi_color_codes_by_color_group" do
+    test "returns the ansi colorcodes grouped by color group" do
+      ansi_color_codes = [
+        %{code: 0, color_group: :gray_and_black},
+        %{code: 1, color_group: :gray_and_black},
+        %{code: 2, color_group: :white},
+        %{code: 3, color_group: :green},
+        %{code: 4, color_group: :blue},
+        %{code: 5, color_group: nil}
+      ]
+
+      ansi_color_codes_by_group = ColorGroup.ansi_color_codes_by_group(ansi_color_codes)
+
+      assert ansi_color_codes_by_group == %{
+               gray_and_black: [0, 1],
+               white: [2],
+               green: [3],
+               blue: [4]
+             }
+    end
+  end
 end
