ee5ae4b3a884e1600b8723bbcb8897d6182e5d8b
Greg Woodward <woodward@users.noreply.github.com>
2024-04-21 19:48:52 -0700

    Show hex code in display of colors in display_color_palette.exs

--- bin/display_color_palette.exs
+++ bin/display_color_palette.exs
@@ -18,14 +18,16 @@ defmodule Display do
     |> Enum.sort()
     |> Enum.map(fn {name, color} ->
       hex = color.ansi_color_code.hex
+      code = color.ansi_color_code.code
 
       IO.puts(
         apply(ColorPalette, name, []) <>
-          "This is color :#{String.pad_trailing(Atom.to_string(name), 26)}  Hex value: ##{hex}" <> reset()
+          "   This is color :#{String.pad_trailing(Atom.to_string(name), 26)}   ANSI Color Code: #{String.pad_leading(Integer.to_string(code), 3)}    Hex value: ##{hex}" <> reset()
       )
     end)
   end
 end
 
+IO.puts("\n\n")
 Display.all()
 Display.stats()
