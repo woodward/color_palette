4400c8a57195133160a9ad297cc1b7a12a64152b
Greg Woodward <woodward@users.noreply.github.com>
2024-04-27 16:20:47 -0700

    Bump version to 0.1.7

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ColorPalette.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/woodward/color_palette"
-  @version "0.1.6"
+  @version "0.1.7"
 
   def project do
     [
